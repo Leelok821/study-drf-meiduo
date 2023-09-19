64828c231e922c4157ade5f950942dc0e07210f8
lee <504455367@qq.com>
2023-09-19 19:45:45 +0800

    111

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -262,3 +262,11 @@ EMAIL_HOST_USER = 'leelok821@163.com'
 EMAIL_HOST_PASSWORD = 'RZVLFEMVZMYHCYOR'
 # 收件⼈看到的发件⼈
 EMAIL_FROM = 'python<itcast99@163.com>'
+
+# DRF扩展
+REST_FRAMEWORK_EXTENSIONS = {
+    # 缓存时间
+    'DEFAULT_CACHE_RESPONSE_TIMEOUT': 60 * 60,
+    # 缓存存储
+    'DEFAULT_USE_CACHE': 'default'
+}
