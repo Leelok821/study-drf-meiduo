529171d427a7e95c8206e096e923172b4a3214fd
lee <504455367@qq.com>
2023-11-29 18:02:54 +0800

    200231129

--- meiduo_mall/meiduo_mall/apps/goods/models.py
+++ meiduo_mall/meiduo_mall/apps/goods/models.py
@@ -1,26 +1,162 @@
 from django.db import models
+from meiduo_mall.utils.models import BaseModel
 
 # Create your models here.
 
-
-class Brand(models):
-    """
-    品牌表
-    """
-    name = models.CharField(max_length=50, verbose_name='名称')
-    logo = models.CharField(max_length=200, verbose_name='logo图片链接')
-    first_letter = models.CharField(max_length=1, verbose_name='首字母')
-
-class GoodsCategory(models):
+class GoodsCategory(BaseModel):
     """
     商品类别表
     """
     name = models.CharField(max_length=10, verbose_name='名称')
-    parent = models.ForeignKey(to='self', on_delete=models.PROTECT, )
+    parent = models.ForeignKey(to='self', null=True, blank=True, on_delete=models.CASCADE, verbose_name='父类')
 
+    class Meta:
+        db_table = 'tb_goods_category'
+        verbose_name = '商品类别'
+        verbose_name_plural = verbose_name
+    
+    def __str__(self):
+        return self.name
 
-class Goods(models):
-    """商品表"""
+class GoodsChannel(BaseModel):
+    """
+    商品频道
+    """
+    group_id = models.IntegerField(null=True, blank=True, verbose_name='所属组')
+    category = models.ForeignKey(GoodsCategory, on_delete=models.CASCADE, verbose_name='顶级商品类别')
+    url = models.CharField(null=True, blank=True, verbose_name='频道页链接地址')
+    sequence = models.IntegerField(verbose_name='组内顺序')
+
+    class Meta:
+        db_table = 'tb_goods_channel'
+        verbose_name = '商品频道'
+        verbose_name_plural = verbose_name
+    
+    def __str__(self):
+        return self.category.name
+
+class Brand(BaseModel):
+    """
+    品牌表
+    """
+    name = models.CharField(max_length=20, verbose_name='名称')
+    logo = models.ImageField(verbose_name='logo图片链接')
+    first_letter = models.CharField(max_length=1, verbose_name='品牌首字母')
+
+    class Meta:
+        db_table = 'tb_brand'
+        verbose_name = '品牌表'
+        verbose_name_plural = verbose_name
+    
+    def __str__(self):
+        return self.name
+
+class Goods(BaseModel):
+    """
+    商品SPU
+    """
     name = models.CharField(max_length=50, verbose_name='名称')
-    brand = models.ForeignKey(to=Brand, on_delete=models.PROTECT, verbose_name='品牌id')
-    category1 = models.ForeignKey()
+    brand = models.ForeignKey(to=Brand, on_delete=models.PROTECT, verbose_name='品牌')
+    category1 = models.ForeignKey(to=GoodsCategory, on_delete=models.PROTECT, related_name='cat1_goods', verbose_name='一级类别')
+    category2 = models.ForeignKey(to=GoodsCategory, on_delete=models.PROTECT, related_name='cat2_goods', verbose_name='二级类别')
+    category3 = models.ForeignKey(to=GoodsCategory, on_delete=models.PROTECT, related_name='cat3_goods', verbose_name='三级类别')
+    sales = models.IntegerField(default=0, verbose_name='销量数')
+    comments = models.IntegerField(default=0, verbose_name='评论数')
+    desc_detail = models.CharField(max_length=500, null=True, blank=True, verbose_name='详细介绍')
+    desc_pack = models.CharField(max_length=500, null=True, blank=True, verbose_name='包装信息')
+    desc_service = models.CharField(max_length=500, null=True, blank=True, verbose_name='售后服务')
+
+    class Meta:
+        db_table = 'tb_goods'
+        verbose_name = '商品表'
+        verbose_name_plural = verbose_name
+    
+    def __str__(self):
+        return self.name
+
+class GoodsSpecification(BaseModel):
+    """
+    商品规格
+    """
+    goods = models.ForeignKey(to=Goods, on_delete=models.CASCADE, verbose_name='商品')
+    name = models.CharField(max_length=20, verbose_name='规格名称')
+
+    class Meta:
+        db_table = 'tb_goods_specification'
+        verbose_name = '商品规格'
+        verbose_name_plural = verbose_name
+    
+    def __str__(self) -> str:
+        return '%s: %s' % (self.goods.name, self.name)
+
+class SpecificationOption(BaseModel):
+    """
+    规则选项
+    """
+    spec = models.ForeignKey(to=GoodsSpecification, on_delete=models.CASCADE, verbose_name='规格')
+    value = models.CharField(max_length=20, verbose_name='选项值')
+
+    class Meta:
+        db_table = 'tb_specification_option'
+        verbose_name = '规格选项'
+        verbose_name_plural = verbose_name
+
+    def __str__(self) -> str:
+        return '%s - %s' % (self.spec, self.value)
+
+class SKU(BaseModel):
+    """
+    商品SKU
+    """
+    name = models.CharField(max_length=50, verbose_name='名称')
+    caption = models.CharField(max_length=100, verbose_name='副标题')
+    goods = models.ForeignKey(to=Goods, on_delete=models.CASCADE, verbose_name='商品')
+    category = models.ForeignKey(to=GoodsCategory, on_delete=models.PROTECT, verbose_name='从属类别')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='单价')
+    cost_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='进价')
+    market_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='市场价格')
+    stock = models.IntegerField(default=0, verbose_name='库存')
+    sales = models.IntegerField(default=0, verbose_name='销量')
+    comments = models.IntegerField(default=0, verbose_name='评论量')
+    is_launched = models.BooleanField(default=True, verbose_name='是否上架')
+    default_image_url = models.CharField(max_length=200, default='', null=True, blank=True, verbose_name='默认图片链接地址')
+
+    class Meta:
+        db_table = 'tb_sku'
+        verbose_name = '商品sku'
+        verbose_name_plural = verbose_name
+    
+    def __str__(self) -> str:
+        return '%s: %s' % (self.id, self.name)
+
+
+class SkuImage(BaseModel):
+    """
+    sku图片
+    """
+    sku = models.ForeignKey(SKU, on_delete=models.CASCADE, verbose_name='所属sku')
+    image = models.ImageField(verbose_name='图片链接地址')
+
+    class Meta:
+        db_table = 'tb_sku_img'
+        verbose_name = 'sku图片'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '%s %s' % (self.sku.name, self.id)
+
+class SkuSpecification(BaseModel):
+    """
+    sku规格信息
+    """
+    sku = models.ForeignKey(to=SKU, on_delete=models.CASCADE, verbose_name='所属sku')
+    spec = models.ForeignKey(to=GoodsSpecification, on_delete=models.PROTECT, verbose_name='规格')
+    option = models.ForeignKey(SpecificationOption, on_delete=models.PROTECT, verbose_name='规格选项')
+
+    class Meta:
+        db_table = 'tb_sku_specification'
+        verbose_name = 'sku规格信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '%s: %s - %s' % (self.sku, self.spec.name, self.option.value)
