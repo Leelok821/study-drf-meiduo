c1e527188f9e99f439c9392c563204c05f8dac2a
lee <504455367@qq.com>
2023-12-04 17:59:54 +0800

    20231204

---
+++ meiduo_mall/meiduo_mall/utils/fastdfs/fastdfs_storage.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+'''
+# Author: li zi hao
+'''
+from django.core.files.storage import Storage
+
+ 
+
+class FastdfsStorage(Storage):
+
+    def _open(self):
+        pass
+
+    def _save(self):
+        pass
+
+    
